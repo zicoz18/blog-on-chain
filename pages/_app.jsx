5e7c991112e31e783864b8c5179de0b165683072
zicoz18 <zicoz18@ku.edu.tr>
2022-01-25 01:16:03 +0300

    Remove Recoil

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,17 +3,14 @@ import 'semantic-ui-css/semantic.min.css'
 import Layout from '../Components/Layout';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   return(
     <div className="pt-4" >
-      <RecoilRoot>
         <Layout>
           <Component {...pageProps} />
           <ToastContainer />
         </Layout>
-      </RecoilRoot>
     </div>
   ) 
 }
