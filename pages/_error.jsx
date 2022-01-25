ac95c2575eb67b48115f74cf5fda775ef539e828
zicoz18 <zicoz18@ku.edu.tr>
2022-01-25 12:50:44 +0300

    Add redirecting loader for wallet connection

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { Button } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Button, Dimmer, Loader, Segment } from "semantic-ui-react";
 
 
 const _error = () => {
     const router = useRouter();
     const [loading, setLoading] = useState(false);
+    const [redirecting, setRedirecting] = useState(false);
+
+    useEffect(() => {
+      const handleRouteChange = (url, { shallow }) => {
+        setRedirecting(true);
+      }
+  
+      router.events.on('routeChangeStart', handleRouteChange)
+  
+      // If the component is unmounted, unsubscribe
+      // from the event with the `off` method:
+      return () => {
+        router.events.off('routeChangeStart', handleRouteChange)
+      }
+    }, [])
 
     const connectWallet = async () => {
         try {
@@ -41,23 +56,31 @@ const _error = () => {
 
 
     const renderNotConnectedContainer = () => (
-        <div className="mb-48 flex flex-col items-center" >
-            <p>You need to connect to your metamask wallet to use this site</p>
-            <Button 
-                onClick={connectWallet} 
-                className="text-center max-h-20"
-                loading={loading}
-                color="teal"
-                >
-              Connect to Wallet
-            </Button>
+      <>
+        <Dimmer active={redirecting}>
+          <Loader content="Redirecting"/>
+        </Dimmer>
+        <div className="flex justify-center items-center min-h-screen" >
+            <div className="mb-48 flex flex-col items-center" >
+                <p>You need to connect your Metamask wallet and use Avax FUJI test network to use this site</p>
+                <Button 
+                    onClick={connectWallet} 
+                    className="text-center max-h-20"
+                    loading={loading}
+                    color="teal"
+                    >
+                  Connect to Wallet
+                </Button>
+            </div>
         </div>
+      </>
     );
 
+
   return (
-    <div className="flex justify-center items-center min-h-screen" >
+      <>
         {renderNotConnectedContainer()}
-    </div>
+      </>
     )
 };
 
