53ee50e77f6f30c8a5de0db1bf8491ad403556df
zicoz18 <zicoz18@ku.edu.tr>
2022-01-25 15:38:00 +0300

    Change favicon and head text

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -1,15 +1,22 @@
 import { Container } from 'semantic-ui-react';
 import Header from './Header';
 import Footer from './Footer';
+import Head from 'next/head';
 
  const Layout = (props) => {
     return (
         /* Container allow us to have spacing at left and right */
+        <>
+        <Head>
+            <title>Blog on Chain</title>
+            <link rel="icon" href="./favicon-32x32.png" />
+        </Head>
         <Container >
             <Header/>
             {props.children}
             <Footer/>
         </Container>
+        </>
     );
  };
 
